25626af77877b4679c65d21c95921c016160c05e
Jason Knight <jason@weareflagship.com>
2013-11-12 19:52:43 -0800

    Add Google Play Services

--- src/com/synthable/wifispy/MainActivity.java
+++ src/com/synthable/wifispy/MainActivity.java
@@ -1,7 +1,12 @@
 package com.synthable.wifispy;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +15,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		int code = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		if(code != ConnectionResult.SUCCESS) {
+			GooglePlayServicesUtil.getErrorDialog(code, this, 0, new OnCancelListener() {
+				@Override
+				public void onCancel(DialogInterface arg0) {
+					finish();
+				}
+			}).show();
+		}
 	}
 
 	@Override
