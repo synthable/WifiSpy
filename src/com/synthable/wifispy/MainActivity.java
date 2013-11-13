b6e1e87dc8f36cb5e30f885ec7a57975a3d69d51
Jason Knight <jason@weareflagship.com>
2013-11-12 19:31:56 -0800

    Clean project

---
+++ src/com/synthable/wifispy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.synthable.wifispy;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
